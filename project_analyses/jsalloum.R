7db8106640b24e52cc1ef0604d97225b891aab20
hawkrobe <rxdh@stanford.edu>
2016-06-24 10:30:22 -0700

    Package jsalloum results:

--- project_analyses/jsalloum.R
+++ project_analyses/jsalloum.R
@@ -1,17 +1,8 @@
-setwd("~/Repos/replication_project/")
-
-loadLibraries = function() {
-  library(tidyr)
-  library(dplyr)
-  library(ggplot2)
-  library(rjson)
-  library(tidyjson)
-  library(lme4)
-  library(lmerTest)
-  library(gridExtra)
-  library(lsr)
-}
-suppressMessages(loadLibraries())
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(rjson)
+library(tidyjson)
 
 path <- "data/jsalloum/"
 files = dir(path, pattern = "*.json")
@@ -42,5 +33,21 @@ d.af = d.raw %>%
   summarise(behaviorScore = mean(behaviorScore, na.rm = T))
 
 # Additive model
-rs1.1 = aov(behaviorScore ~ plev * speakerSex, data = d.af)
-summary(rs1.1)
+rs1.1 = lm(behaviorScore ~ plev + speakerSex, data = d.af)
+
+# Extract params
+params = anova(rs1.1)
+F_stat = params[1,4]
+p = params[1,5]
+dfnum = params$Df[1]
+dfdenom = params$Df[length(params$Df)]
+stat_descript <- paste0("F(",dfnum,", ",dfdenom,") = ",round(F_stat, 3))
+stat_descript
+
+project_info <- data.frame(
+  project_key = "jsalloum", 
+  rep_final_n = length(d.af$speakerId), 
+  rep_n_excluded = 0, 
+  rep_es = F_stat, 
+  rep_test_statistic = stat_descript,
+  rep_p_value = p)
